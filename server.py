533b3625e97a87e0767d45b2df0841a6e18242ac
Paul Pronio <paul.pronio@hotmail.com>
2025-09-22 17:51:27 -0400

    Task 8 Complete

--- server.py
+++ server.py
@@ -1,17 +1,17 @@
 """
 This is a server to respond the Emotion Detection of a phrase
 """
-from flask import Flask, render_template, request, jsonify, make_response
+from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detection")
 
 @app.route("/emotionDetector")
-"""
-Emotion Detector will take a string and use the watson AI to
-Determine the emotion behind it. 
-"""
 def sent_analyzer():
+    """
+    Emotion Detector will take a string and use the watson AI to
+    Determine the emotion behind it. 
+    """
     # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
     # Pass the text to the sentiment_analyzer function and store the response
@@ -26,15 +26,18 @@ def sent_analyzer():
 
     # Check if the label is None, indicating an error or invalid input
     if em is None:
-        return "Invalid text! Please try again!"
+        message =  "Invalid text! Please try again!"
     else:
-        return f"""For the given statement, the system response is 'anger': {anger},
+        message =  f"""For the given statement, the system response is 'anger': {anger},
     'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}.
     The dominant emotion is {em}.
     """
-
+    return message
 @app.route("/")
 def render_index_page():
+    """
+    Renders the index page
+    """
     return render_template('index.html')
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
